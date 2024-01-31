7e484bfee15b725445ad20838347865b0c3d5d17
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-01-31 20:29:01 +0300

    del logger.warning('Нет ответа от сервера')

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def main():
                     bot.send_message(chat_id=tg_chat_id, text=message)
                 timestamp = review_response['last_attempt_timestamp']
 
-        except requests.exceptions.ReadTimeout:
-            logger.warning('Нет ответа от сервера')
+#        except requests.exceptions.ReadTimeout:
+#            logger.warning('Нет ответа от сервера')
 
         except requests.exceptions.ConnectionError:
             logger.warning('Потеряно интернет соединение')
